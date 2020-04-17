55a35dd2c57bdc4e268d6ba096c5ba179d90f54a
jianglianEin <913057041@qq.com>
2020-04-17 11:59:22 +0800

    [JiShi] change restTemplate unicode from default to utf-8.

--- src/main/kotlin/com/jianglianein/apigateway/config/RestTemplateConfig.kt
+++ src/main/kotlin/com/jianglianein/apigateway/config/RestTemplateConfig.kt
@@ -4,15 +4,18 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.ClientHttpRequestFactory
 import org.springframework.http.client.SimpleClientHttpRequestFactory
+import org.springframework.http.converter.StringHttpMessageConverter
 import org.springframework.web.client.RestTemplate
-
+import java.nio.charset.StandardCharsets
 
 
 @Configuration
 class RestTemplateConfig {
     @Bean
     fun restTemplate(factory: ClientHttpRequestFactory?): RestTemplate {
-        return RestTemplate(factory!!)
+        val restTemplate = RestTemplate(factory!!)
+        restTemplate.messageConverters.set(1, StringHttpMessageConverter(StandardCharsets.UTF_8))
+        return restTemplate
     }
 
     @Bean
